f261174f88978dab5dd2a30b8cdefa2b81927ea6
Josh Dluehosh <joshd@Admins-MacBook-Pro.local>
2019-10-09 12:27:05 -0700

    added realtime updates forposts and comments

--- logic.js
+++ logic.js
@@ -15,170 +15,109 @@ const db = firebase.database().ref();
 const posts = db.child("posts");
 const comments = db.child("comments");
 
-posts.once("value", function(snapshot) {
-  const res = snapshot.val();
-  const content = Object.values(res);
+posts.on("child_added", function(snapshot) {
+  const data = snapshot.val();
+  const body = data.body;
+  const result = document.getElementById("result");
+  const container = document.createElement("div");
+  const heading = document.createElement("h2");
+  const comment = document.createElement("p");
+  const feedContain = document.createElement("div");
+  const feedback = document.createElement("input");
+  const feedBtn = document.createElement("button");
+  const collapse = document.createElement("button");
 
-  content.map((data, index) => {
-    const body = data.body;
-    const result = document.getElementById("result");
-    const container = document.createElement("div");
-    const heading = document.createElement("h2");
-    const comment = document.createElement("p");
-    const feedContain = document.createElement("div");
-    const feedback = document.createElement("input");
-    const feedBtn = document.createElement("button");
-    const collapse = document.createElement("button");
+  container.className = "postBox w-70 pa3 tl center";
+  container.id = `postBox${data.postNum}`;
+  heading.className = "heading tl";
+  heading.id = `heading${data.postNum}`;
+  comment.className = "comment";
+  comment.id = `comment${data.postNum}`;
+  feedContain.className = "commentBox";
+  feedBtn.innerHTML = "Add Comment";
+  feedBtn.id = `addComment${data.postNum}`;
+  feedback.id = `replyBox${data.postNum}`;
+  feedback.className = "txtBox";
+  feedBtn.className = "txtBtn";
+  collapse.className = "collapsible";
+  collapse.id = `collapsible${data.postNum}`
+  collapse.innerHTML = "Click to Add/View Comments";
 
-    container.className = "postBox w-70 pa3 tl center";
-    container.id = `postBox${index + 1}`;
-    heading.className = "heading tl";
-    heading.id = `heading${index + 1}`;
-    comment.className = "comment";
-    comment.id = `comment${index + 1}`;
-    feedContain.className = "commentBox";
-    feedBtn.innerHTML = "Add Comment";
-    feedBtn.id = `addComment${index + 1}`;
-    feedback.id = `replyBox${index + 1}`;
-    feedback.className = "txtBox";
-    feedBtn.className = "txtBtn";
-    collapse.className = "collapsible";
-    collapse.innerHTML = "Click to Add/View Comments";
+  const title = document.createTextNode(`Post #${data.postNum}`);
+  let input;
 
-    const title = document.createTextNode(`Post #${index + 1}`);
-    let input = body;
-
-    if (body.substring(0, 4) === "pic:" || body.substring(0, 4) === "Pic:") {
-      input = document.createElement("img");
-      input.src = body.substring(4);
-      input.className = "pics";
-    } else {
-      input = document.createTextNode(body);
-    }
-
-    heading.appendChild(title);
-    comment.appendChild(input);
-    container.appendChild(comment);
-    comment.before(heading);
-    feedContain.appendChild(feedback);
-    feedContain.appendChild(feedBtn);
-    container.appendChild(collapse);
-    container.appendChild(feedContain);
-    result.appendChild(container);
-
-    const box = document.getElementById(`addComment${index + 1}`);
-    box.addEventListener(
-      "click",
-      () => {
-        const reply = document.getElementById(`replyBox${index + 1}`).value;
-        if (reply.trim() !== "") {
-          comments.once("value", function(snapshot) {
-
-            const post = document.createElement("p");
-            post.className = "reply";
-            const input = document.createTextNode(reply);
-            comments.push({ forPost: index + 1, body: reply });
-            post.appendChild(input);
-            feedback.before(post);
-            document.getElementById(`replyBox${index + 1}`).value = "";
-          });
-        } else {
-          alert("You can't post nothing, bruh.");
-        }
-      },
-      false
-    );
-  });
-
-  const coll = document.getElementsByClassName("collapsible");
-  let i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      const content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
+  if (body.substring(0, 4) === "pic:" || body.substring(0, 4) === "Pic:") {
+    input = document.createElement("img");
+    input.src = body.substring(4);
+    input.className = "pics";
+  } else {
+    input = document.createTextNode(body);
   }
-});
 
-comments.once("value", function(snapshot) {
-  const res = snapshot.val();
-  const content = Object.values(res);
+  heading.appendChild(title);
+  comment.appendChild(input);
+  container.appendChild(comment);
+  comment.before(heading);
+  feedContain.appendChild(feedback);
+  feedContain.appendChild(feedBtn);
+  container.appendChild(collapse);
+  container.appendChild(feedContain);
+  result.appendChild(container);
 
-  content.map((data, index) => {
-    const body = data.body;
-    const post = document.createElement("p");
-    post.className = "reply";
-    const input = document.createTextNode(body);
-    post.appendChild(input);
-    const box = document.getElementById(`replyBox${data.forPost}`);
-    box.before(post);
+  const coll = document.getElementById(`collapsible${data.postNum}`);
+  coll.addEventListener("click", function() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
   });
+
+  const box = document.getElementById(`addComment${data.postNum}`);
+  box.addEventListener(
+    "click",
+    () => {
+      const reply = document.getElementById(`replyBox${data.postNum}`).value;
+      if (reply.trim() !== "") {
+        comments.push({ forPost: data.postNum, body: reply });
+        document.getElementById(`replyBox${data.postNum}`).value = "";
+      } else {
+        alert("You can't post nothing, bruh.");
+      }
+    },
+    false
+  );
 });
 
-const getComment = () => {
-  const msg = document.getElementById("msg").value;
+comments.on("child_added", function(snapshot) {
+  const data = snapshot.val();
+  const content = Object.values(data);
 
+  const body = data.body;
+  const post = document.createElement("p");
+  post.className = "reply";
+  const input = document.createTextNode(body);
+  post.appendChild(input);
+  const box = document.getElementById(`replyBox${data.forPost}`);
+  box.before(post);
+});
+
+const createPost = () => {
+  const msg = document.getElementById("msg").value;
   if (msg.trim() !== "") {
     posts.once("value", function(snapshot) {
       const res = snapshot.val();
       const content = Object.values(res);
       const postCt = content.length + 1;
-
       posts.push({ postNum: postCt, body: msg, commentCT: 0 });
-
-      const result = document.getElementById("result");
-      const container = document.createElement("div");
-      const heading = document.createElement("h2");
-      const comment = document.createElement("p");
-      const feedContain = document.createElement("div");
-      const feedback = document.createElement("input");
-      const feedBtn = document.createElement("button");
-
-      container.className = "postBox w-70 pa3 tl center";
-      container.id = `postBox${postCt}`;
-      heading.className = "heading tl";
-      heading.id = `heading${postCt}`;
-      comment.className = "comment";
-      comment.id = `comment${postCt}`;
-      feedContain.className = "commentBox";
-      feedBtn.innerHTML = "Add Comment";
-      feedBtn.id = `addComment${postCt}`;
-      feedback.id = `replyBox${postCt}`;
-      feedback.className = "txtBox";
-      feedBtn.className = "txtBtn";
-
-      const title = document.createTextNode(`Post #${postCt}`);
-      let input = msg;
-
-      if (msg.substring(0, 4) === "pic:" || msg.substring(0, 4) === "Pic:") {
-        input = document.createElement("img");
-        input.src = msg.substring(4);
-        input.className = "pics";
-      } else {
-        input = document.createTextNode(msg);
-      }
-
-      heading.appendChild(title);
-      comment.appendChild(input);
-      container.appendChild(comment);
-      comment.before(heading);
-      feedContain.appendChild(feedback);
-      feedContain.appendChild(feedBtn);
-      container.appendChild(feedContain);
-      result.appendChild(container);
-
-      document.getElementById("msg").value = "";
     });
+    document.getElementById("msg").value = "";
   } else {
     alert("You can't post nothing, bruh.");
   }
 };
 
 const post = document.getElementById("post");
-post.addEventListener("click", getComment, false);
+post.addEventListener("click", createPost, false);
